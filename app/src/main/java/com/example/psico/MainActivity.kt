4ac1fdb92bf13079244b09410de2e532fff6129d
alami <alamillagmanuel7@gmail.com>
2024-10-24 03:51:58 -0600

    Diseño terminado, mas enlace a ventana y musica de fondo

--- app/src/main/java/com/example/psico/MainActivity.kt
+++ app/src/main/java/com/example/psico/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.psico
 
+import android.media.MediaPlayer
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        var mediaPlayer = MediaPlayer.create(this, R.raw.inner_light)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnGoToRegister: Button = findViewById(R.id.btnCreateAccount)
+        btnGoToRegister.setOnClickListener {
+            // Crear un Intent para ir a RegisterActivity
+            val intent = Intent(this, RegistrerActivity::class.java)
+            startActivity(intent)
+        }
+        fun onDestroy() {
+            super.onDestroy()
+            // Libera el MediaPlayer para evitar fugas de memoria
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+            }
+            mediaPlayer.release()
         }
     }
 }
