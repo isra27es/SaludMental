ac608c1858158723ac3dddbe5a7dfb41f3e48803
Chava364 <espinozasalvador364@gmail.com>
2024-11-03 14:51:06 -0600

    Quitar Main para poder actualizar

--- app/src/main/java/com/example/psico/MainActivity.kt
+++ app/src/main/java/com/example/psico/MainActivity.kt
@@ -5,12 +5,5 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        // Inicia DiarioActivity directamente
-        val intent = Intent(this, DIarioActivity::class.java)
-        startActivity(intent)
-        finish() // Cierra MainActivity para que no esté en la pila de actividades
-    }
 }
